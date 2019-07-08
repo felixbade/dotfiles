429716e7d75f258945973ddb879ae4308d0a2b53
Felix Bade <flix.bade@gmail.com>
2019-07-08 21:48:57 +0300

    Enable desktop picture changing

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,6 +1,6 @@
 title "Desktop"
 echo " - Set wallpaper to wallpaper-pallurat.png"
-#sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$DIR/resources/wallpaper-pallurat.png'";
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$DIR/resources/wallpaper-pallurat.png'";
 
 echo " - Hide desktop items"
 defaults write com.apple.finder CreateDesktop -bool false
